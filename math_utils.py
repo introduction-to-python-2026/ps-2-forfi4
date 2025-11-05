2ee1dca1a56f88e56ad99532f1683010c56b5899
forfi4 <liamipomi@gmail.com>
2025-11-05 09:23:08 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -9,6 +9,6 @@ def find_mean(num1, num2, num3):
   return mean
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    std = (((num1 - mean) ** 2) + ((num2 - mean) ** 2) + ((num3 - mean) ** 2)) **0.5
+    std = ((num1 - mean) ** 2 + ((num2 - mean) ** 2) + ((num3 - mean) ** 2)) **0.5
     return mean, std
 
